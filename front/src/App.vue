253bb67c34171560f4b7e4a4ce9957c91d22a7c3
Stanislav <sta-tun-slav@yandex.ru>
2024-08-31 09:53:49 +0300

    feat: :fire: init project

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="text-white">
+    <NavBar />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import NavBar from "./components/NavBar.vue";
 
-export default {
-  name: 'App',
+@Options({
   components: {
-    HelloWorld
-  }
-}
+    NavBar,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
@@ -21,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ front/src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="w-full h-14 bg-black flex flex-row justify-between items-center">
+    <div class="w-12 h-12">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="{2}"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 19.5 8.25 12l7.5-7.5"
+        />
+      </svg>
+    </div>
+
+    <div class="flex flex-col">
+      <span class="text-lg"> Клубные боты </span>
+      <span class="text-gray-700"> боты </span>
+    </div>
+
+    <div class="w-14 h-14 flex items-center justify-center">
+      <div class="w-10 h-10 bg-white rounded-full"></div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>

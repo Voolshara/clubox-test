e1eb769a4fb55b4487220b3cc8fbd1326c984055
Stanislav <sta-tun-slav@yandex.ru>
2024-09-01 11:47:58 +0300

    add pg as docker

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,10 +1,13 @@
 import { useUserDataStore } from "../store";
 import { userDataType } from "../types/user.types";
 
+// const apiLink = "http://127.0.0.1:8000";
+const apiLink = "https://webappapi.tungulov.space";
+
 export const getUserData = async (tg_id: String) => {
   const store = useUserDataStore();
 
-  const response = await fetch(`http://127.0.0.1:8000/user/${tg_id}`);
+  const response = await fetch(`${apiLink}/user/${tg_id}`);
   if (response.status === 200) {
     const data = (await response.json()) as userDataType;
     store.setUserData(data);

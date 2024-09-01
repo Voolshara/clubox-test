85e72f1ae2b1d54b6cf2d4dcc244e624d2eab580
Stanislav <sta-tun-slav@yandex.ru>
2024-09-01 14:15:29 +0300

    test: :art: app is up ))

--- front/src/App.vue
+++ front/src/App.vue
@@ -28,9 +28,9 @@ export default {
       const tg_id = (initData as string).split("tg_user-")[1].split("&")[0];
       getUserData(tg_id);
     } else {
-      // const { id } = (window as unknown as TelegrammedWindow).Telegram.WebApp
-      //   .initDataUnsafe.user;
-      getUserData("111");
+      const { id } = (window as unknown as TelegrammedWindow).Telegram.WebApp
+        .initDataUnsafe.user;
+      getUserData(id);
     }
     return { userData };
   },
